191730ad6034d03ea3733eacb8e913edc260d34a
Navi-Arh <naviarh@mail.ru>
2018-12-20 23:19:53 +0300

    Edit install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 go get -u github.com/naviarh/goget
 cd $GOPATH/src/github.com/naviarh/goget
-go build
-strip -sxv goget
+go build --ldflags="-s -w"
+strip -s -x -v goget
 upx -9v goget
 cp ./goget $GOPATH/bin
 echo
